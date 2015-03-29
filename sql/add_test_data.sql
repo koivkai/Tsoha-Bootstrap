6c7cce254e09bd2516b5d104fb508f5da79657d7
koivkai <kaius.koivunen@gmail.com>
2015-03-29 16:42:50 +0300

    asdf

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,6 +1,8 @@
-INSERT INTO Poll (name, endDate, startDate) VALUES ('Testi', date(03.04.2015), date(05.06.2015));
+INSERT INTO Poll (name, endDate, startDate) VALUES ('Testi', '2015-08-07', '2015-09-08');
 
 
 INSERT INTO Voter (voterName, password, firstName, lastName, email) VALUES ('SnowMan99', 'salasana', 'John', 'Snow', 'JSnow@lolcats.com');
 
 INSERT INTO Options (optionName, optionDesc, votesReceived, parentPoll) VALUES ('Pepsi', 'Pepsi, normaali pepsi ei light, zero tai muuta roskaa', 2000, 1);
+
+INSERT INTO Vote (caster,castIn, castDate) VALUES (1, 1, '2015-03-22');
